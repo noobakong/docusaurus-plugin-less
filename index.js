f40f0a48fc58fc390a02ed6c892ecdece13cd18b
Akong <noobakong@163.com>
2023-06-29 16:39:00 +0800

    Update index.js add hash

--- index.js
+++ index.js
@@ -1,56 +1,50 @@
-const { name } = require("./package.json");
-
-module.exports = function (context, options)
-{
-    const { id, ...pluginOptions } = options;
-    return {
-        name,
-        configureWebpack(config, isServer, utils)
-        {
-            const { getStyleLoaders } = utils;
-            const isProd = process.env.NODE_ENV === "production";
-            return {
-                module: {
-                    rules: [
-                        {
-                            test: /\.less$/,
-                            oneOf: [
-                                {
-                                    test: /\.module\.less$/,
-                                    use: [
-                                        ...getStyleLoaders(
-                                            isServer,
-                                            {
-                                                modules: {
-                                                    localIdentName: isProd
-                                                        ? "[sha1:hash:hex:5]"
-                                                        : "[name]_[local]",
-                                                    exportOnlyLocals: isServer
-                                                },
-                                                importLoaders: 1,
-                                                sourceMap: !isProd
-                                            }
-                                        ),
-                                        {
-                                            loader: "less-loader",
-                                            options: pluginOptions || {}
-                                        }
-                                    ]
-                                },
-                                {
-                                    use: [
-                                        ...getStyleLoaders(isServer),
-                                        {
-                                            loader: "less-loader",
-                                            options: pluginOptions || {}
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
+module.exports = function (context, options) {
+  const { id, ...pluginOptions } = options;
+  return {
+    name: "my-less-loader",
+    configureWebpack(config, isServer, utils) {
+      const { getStyleLoaders } = utils;
+      const isProd = process.env.NODE_ENV === "production";
+      return {
+        module: {
+          rules: [
+            {
+              test: /\.less$/,
+              oneOf: [
+                {
+                  test: /\.module\.less$/,
+                  use: [
+                    ...getStyleLoaders(
+                      isServer,
+                      {
+                        modules: {
+                          localIdentName: "[name]_[local]_[hash:base64:5]",
+                          exportOnlyLocals: isServer
+                        },
+                        importLoaders: 1,
+                        sourceMap: !isProd
+                      }
+                    ),
+                    {
+                      loader: "less-loader",
+                      options: pluginOptions || {}
+                    }
+                  ]
+                },
+                {
+                  use: [
+                    ...getStyleLoaders(isServer),
+                    {
+                      loader: "less-loader",
+                      options: pluginOptions || {}
+                    }
+                  ]
                 }
-            };
+              ]
+            }
+          ]
         }
-    };
+      };
+    }
+  };
 };
